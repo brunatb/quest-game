4b72672d3d2a2d175c929d0a36193fb3cae6e992
Jean <jean.sousa6@outlook.com>
2024-10-08 15:18:59 -0300

    Criando um componente para o logo e reajustando paginas

--- src/app/[roomId]/sala-de-espera/page.tsx
+++ src/app/[roomId]/sala-de-espera/page.tsx
@@ -1,10 +1,13 @@
 import { CopyRoomCode, WaitingPlayerContainer } from "./components";
 import { GoBackButton } from "@/app/components";
-
+import { LogoQueston } from "@/app/components/logo";
 
 export default function Page({ params }: Props) {
   return (
     <div className="space-y-4">
+      <div className="flex flex-col items-center space-y-2">
+        <LogoQueston/>
+      </div>
       <GoBackButton href="/">Voltar</GoBackButton>
       <h1 className="font-[family-name:var(--font-geist-mono)] w-full text-center text-2xl font-bold">
         Sala de espera
--- src/app/criar-sala/page.tsx
+++ src/app/criar-sala/page.tsx
@@ -1,18 +1,11 @@
 import { GoBackButton } from "../components";
+import { LogoQueston } from "../components/logo";
 import { CreateRoomForm } from "./components";
-import Image from "next/image";
-
-
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center max-w-full lg:w-1/3 space-y-2">
-      <Image 
-        src="/images/Logo4.png"
-        width={400}
-        height={400}
-        alt="Picture of the author"
-      />
+      <LogoQueston/>
       <div className="w-full text-left">
         <GoBackButton href="/">Voltar</GoBackButton>
       </div>
