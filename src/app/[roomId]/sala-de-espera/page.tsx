6f69e3c55f2455ad467f5b359b3ea811252b5dad
Jean <jean.sousa6@outlook.com>
2024-10-20 16:25:09 -0300

    ajustando tela de espera

--- src/app/[roomId]/sala-de-espera/page.tsx
+++ src/app/[roomId]/sala-de-espera/page.tsx
@@ -1,19 +1,16 @@
-import { CopyRoomCode, WaitingPlayerContainer } from "./components";
-import { GoBackButton } from "@/app/components";
 import { LogoQueston } from "@/app/components/logo";
-
+import { WaitingPlayerContainer } from "./components";
+import { CopyRoomCode } from "./components";
 export default function Page({ params }: Props) {
   return (
-    <div className="space-y-4">
-      <div className="flex flex-col items-center space-y-2">
-        <LogoQueston/>
-      </div>
-      <GoBackButton href="/">Voltar</GoBackButton>
-      <h1 className="font-[family-name:var(--font-geist-mono)] w-full text-center text-2xl font-bold">
-        Sala de espera
-      </h1>
+    <div className=" flex flex-col p-8">
+     <LogoQueston/>
+      <div className="space-y-8 font-poppins w-full text-center text-xl font-bold">
+        <WaitingPlayerContainer/>
+        <div className="p-4">
       <CopyRoomCode roomId={params.roomId} />
-      <WaitingPlayerContainer />
+        </div>
+      </div>
     </div>
   );
 }
